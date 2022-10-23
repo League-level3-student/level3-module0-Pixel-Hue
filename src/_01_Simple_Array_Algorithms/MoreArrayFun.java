d3386b635b9a9ed7639e93b51c220c30a4c5f095
GavinB <GavinB@hi.attlocal.net>
2022-10-23 16:28:50 -0700

    sdfgdfshfhasdhfasdfsdaf

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -1,13 +1,15 @@
 package _01_Simple_Array_Algorithms;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
 public static void main(String[] args) {
-	String[] arr= {"1", "2", "3", "4", "5"};
-	
+	String[] arr= {"a", "b", "c", "d", "e"};
+
 	printS(arr);
 	System.out.println("---");
 	RevPrintS(arr);
@@ -15,7 +17,7 @@ public static void main(String[] args) {
 	EOString(arr);
 	System.out.println("---");
 	RandomOrder(arr);
-	System.out.println("---");
+
 }
 
 
@@ -45,14 +47,36 @@ void EOString(String[] array) {
 
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
-static Random ran = new Random();
-
+    static Random ran = new Random();
+    static int check = 0;
+    static ArrayList<int> UsedInt = new ArrayList<int>();
+    //make arraylist int
 static void RandomOrder(String[] array) {
+	int numPrinted = 0;
+	while (numPrinted < 5) {
+		
+	
+	
+	
 	for (int i = 0; i < array.length; i++) {
 		int e = ran.nextInt(array.length);
-		System.out.println(array[e]);
+		System.out.println(e);
+		for (int k = 0; k < UsedInt.size(); k++) {
+			int checkNum = Integer.parseInt(UsedInt.get(k));
+			
+			if (e != checkNum) {
+				check++;
+		}
+			}
+		if (check== UsedInt.size()) {
+			System.out.println(array[e]);
+			numPrinted++;
+		}
+			UsedInt.add(e);
+		}
+		
 		//remove array that was printed to use all numbers
 	}
 }
-
 }
+
